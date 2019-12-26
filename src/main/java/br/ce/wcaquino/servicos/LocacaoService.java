dde45a102a0b194f35f15d7d58c279781a8b5522
Thiago André <thiago.silva@eletraenergy.com>
2019-12-26 17:39:36 -0300

    Modificado filme para lista de filmes nas clases Locacao e LocacaoService. Obs: Commit com erro de código nos testes.

--- src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -3,6 +3,7 @@ package br.ce.wcaquino.servicos;
 import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
 
 import java.util.Date;
+import java.util.List;
 
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
@@ -11,31 +12,40 @@ import br.ce.wcaquino.exception.FilmeSemEstoqueException;
 import br.ce.wcaquino.exception.LocadoraException;
 
 public class LocacaoService {
-	
-	public Locacao alugarFilme(Usuario usuario, Filme filme) throws FilmeSemEstoqueException, LocadoraException {
-		if (usuario == null){
-			throw new LocadoraException("Usuário vazio");
-		}
-		if (filme == null){
-			throw new LocadoraException("Filme vazio");
-		}
-		if (filme.getEstoque() == 0){
-			throw new FilmeSemEstoqueException();
-		}
-		Locacao locacao = new Locacao();
-		locacao.setFilme(filme);
-		locacao.setUsuario(usuario);
-		locacao.setDataLocacao(new Date());
-		locacao.setValor(filme.getPrecoLocacao());
-		
-		//Entrega no dia seguinte
-		Date dataEntrega = new Date();
-		dataEntrega = adicionarDias(dataEntrega, 1);
-		locacao.setDataRetorno(dataEntrega);
-		
-		//Salvando a locacao...	
-		//TODO adicionar método para salvar
-		
-		return locacao;
-	}
+
+    public Locacao alugarFilme(Usuario usuario, List<Filme> listaFilme) throws FilmeSemEstoqueException, LocadoraException {
+        if (usuario == null) {
+            throw new LocadoraException("Usuário vazio");
+        }
+        if (listaFilme.isEmpty()) {
+            throw new LocadoraException("Filme vazio");
+        }
+        for (Filme lista : listaFilme) {
+            if (lista.getEstoque() == null) {
+                throw new FilmeSemEstoqueException();
+            }
+        }
+
+        Locacao locacao = new Locacao();
+        locacao.setListaFilme(listaFilme);
+        locacao.setUsuario(usuario);
+        locacao.setDataLocacao(new Date());
+
+        // adicionar preço da locação
+        double valorLocacaoAux = 0;
+        for (Filme lista : listaFilme) {
+            valorLocacaoAux += lista.getPrecoLocacao();
+        }
+        locacao.setValor(valorLocacaoAux);
+
+        //Entrega no dia seguinte
+        Date dataEntrega = new Date();
+        dataEntrega = adicionarDias(dataEntrega, 1);
+        locacao.setDataRetorno(dataEntrega);
+
+        //Salvando a locacao...
+        //TODO adicionar método para salvar
+
+        return locacao;
+    }
 }
