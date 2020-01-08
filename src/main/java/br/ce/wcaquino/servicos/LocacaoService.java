9dd15f850739549bc92c5823c3b8c111f9395ff4
Thiago André Cardoso Silva <thiagoandrecardoso@icloud.com>
2020-01-08 10:08:43 -0300

    Builder master + LocacaoDAO
    Obs: Tests are failing due to lack of data persistence 
    Added interface locacaoDAO with save method

--- src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -6,6 +6,7 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
+import br.ce.wcaquino.daos.LocacaoDAO;
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
@@ -14,6 +15,8 @@ import br.ce.wcaquino.exception.LocadoraException;
 import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoService {
+
+    private LocacaoDAO dao;
 
     public Locacao alugarFilme(Usuario usuario, List<Filme> listaFilme) throws FilmeSemEstoqueException, LocadoraException {
         if (usuario == null) {
@@ -67,7 +70,7 @@ public class LocacaoService {
         }
         locacao.setDataRetorno(dataEntrega);
         //Salvando a locacao...
-        //TODO adicionar método para salvar
+        dao.salvar(locacao);
 
         return locacao;
     }
