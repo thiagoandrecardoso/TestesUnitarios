c1c887c435a7d5d969a61ad79b40ff92f9218baa
Thiago André Cardoso Silva <thiagoandrecardoso@icloud.com>
2020-02-16 08:43:44 -0300

    useUnlimitedThreads true in pon

--- src/test/java/br/ce/wcaquino/servicos/CalculadoraTest.java
+++ src/test/java/br/ce/wcaquino/servicos/CalculadoraTest.java
@@ -1,42 +1,49 @@
 package br.ce.wcaquino.servicos;
 
-import br.ce.wcaquino.runners.ParallelRunner;
 import org.junit.*;
-import org.junit.runner.RunWith;
 
-@RunWith(ParallelRunner.class)
+//@RunWith(ParallelRunner.class)
 public class CalculadoraTest {
+
+    public static StringBuffer ordem = new StringBuffer();
+
     Calculadora calculadora;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         calculadora = new Calculadora();
         System.out.println("iniciando");
     }
 
     @Test
-    public void deveSomarDoisNumeros(){
+    public void deveSomarDoisNumeros() {
         Assert.assertEquals(5, calculadora.somar(2, 3));
     }
 
     @Test
-    public void deveSomarDoisNumeros1(){
+    public void deveSomarDoisNumeros1() {
         Assert.assertEquals(5, calculadora.somar(2, 3));
     }
 
     @Test
-    public void deveSomarDoisNumeros2(){
+    public void deveSomarDoisNumeros2() {
         Assert.assertEquals(5, calculadora.somar(2, 3));
     }
 
     @Test
-    public void deveSomarDoisNumeros3(){
+    public void deveSomarDoisNumeros3() {
         Assert.assertEquals(5, calculadora.somar(2, 3));
     }
 
 
     @After
-    public void tearDown(){
+    public void tearDown() {
         System.out.println("finalizando");
+        ordem.append("1");
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+        System.out.println(ordem);
     }
 }
